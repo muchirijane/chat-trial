aa9c70793354ab0cab510a8411a3cdda6dceac66
jane muthoni <muchirijane@outlook.com>
2020-07-08 14:42:36 +0300

    setting up output for user and chatbot messages

--- app.js
+++ app.js
@@ -5,11 +5,28 @@ let chatareaouter = document.querySelector('.chatarea-outer');
 const speechRecognition = window.speechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
 
-recognition.onresult = function(e){
+function showUserMsg(userMsg) {
+    let html = '';
+    html += `<div class="chatarea-inner user">${userMsg}</div>`;
+    chatareaouter.innerHTML += html;
+    return chatareaouter;
+}
+
+const showChatbot = (userChatbot) => {
+    let html = '';
+    html += ` <div class="chatarea-inner chatbot">${userChatbot}</div>`;
+    chatareaouter.innerHTML += html;
+    return chatareaouter;
+}
+
+recognition.onresult=function(e){
+    let resultIndex = e.resultIndex;
+    let transcript = e.results[resultIndex][0].transcript;
+    console.log(transcript);
     console.log(e)
 }
 
-mic.addEventListener('click', function ()  {
+mic.addEventListener("click", function(){
     recognition.start();
-    console.log('ACTIVETED!!');
+    console.log("Activated");
 })
